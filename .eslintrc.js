3ed6097c0a9503355e5a93f1647e9cc09ed29add
Keerthana Ramakrishnan <keerthana.r@thoughtworks.com>
2024-01-31 15:50:19 +0530

    Add rule to ignore import react

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,5 +21,10 @@ module.exports = {
         sourceType: 'module',
     },
     plugins: ['@typescript-eslint', 'react'],
-    rules: {},
+    rules: {
+        // suppress errors for missing 'import React' in files
+        'react/react-in-jsx-scope': 'off',
+        // allow jsx syntax in js files (for next.js project)
+        'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }], //should add ".ts" if typescript project
+    },
 }
